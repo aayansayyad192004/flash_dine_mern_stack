a435e525fd14d2940d91b80874f562047c3646ab
aayansayyad192004 <aayansayyad19@gmail.com>
2025-04-10 10:19:29 +0530

    project updated

--- frontend/src/components/CheckoutButton.tsx
+++ frontend/src/components/CheckoutButton.tsx
@@ -34,13 +34,7 @@ const CheckoutButton = ({ onCheckout, disabled, isLoading }: Props) => {
     });
   };
 
-  if (!isAuthenticated) {
-    return (
-      <Button onClick={onLogin} className="bg-orange-500 flex-1">
-        Log in to check out
-      </Button>
-    );
-  }
+  
 
   if (isAuthLoading || !currentUser || isLoading) {
     return <LoadingButton />;
