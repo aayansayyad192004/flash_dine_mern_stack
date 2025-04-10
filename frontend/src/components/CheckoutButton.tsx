b72d32c72662a12b9fff508bbf20ee73391041ba
aayansayyad192004 <aayansayyad19@gmail.com>
2025-04-10 23:58:05 +0530

    project updated

--- frontend/src/components/CheckoutButton.tsx
+++ frontend/src/components/CheckoutButton.tsx
@@ -1,59 +1,26 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import { useLocation } from "react-router-dom";
-import { Button } from "./ui/button";
-import LoadingButton from "./LoadingButton";
-import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
-import UserProfileForm, { UserFormData } from "@/forms/user-profile-form/UserProfileForm";
-import { useGetMyUser } from "@/api/MyUserApi";
+import { useEffect, useRef } from "react";
 
-type Props = {
-  onCheckout: (userFormData: UserFormData) => void;
-  disabled: boolean;
-  isLoading: boolean;
-};
+const CheckoutButton = () => {
+  const scriptContainerRef = useRef<HTMLDivElement>(null);
 
-const CheckoutButton = ({ onCheckout, disabled, isLoading }: Props) => {
-  const { isAuthenticated, isLoading: isAuthLoading, loginWithRedirect } = useAuth0();
-  const { pathname } = useLocation();
-  const { currentUser, isLoading: isGetUserLoading } = useGetMyUser();
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://checkout.razorpay.com/v1/payment-button.js";
+    script.setAttribute("data-payment_button_id", "pl_QHS8pZKyf4PAGY");
+    script.async = true;
 
-  const onLogin = async () => {
-    await loginWithRedirect({
-      appState: {
-        returnTo: pathname,
-      },
-    });
-  };
-
-  if (!isAuthenticated) {
-    return (
-      <Button onClick={onLogin} className="bg-orange-500 flex-1">
-        Log in to check out
-      </Button>
-    );
-  }
-
-  if (isAuthLoading || !currentUser || isLoading) {
-    return <LoadingButton />;
-  }
+    if (scriptContainerRef.current) {
+      scriptContainerRef.current.innerHTML = ""; // Clear any previous scripts
+      scriptContainerRef.current.appendChild(script);
+    }
+  }, []);
 
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button disabled={disabled} className="bg-orange-500 flex-1">
-          Go to checkout
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="max-w-[425px] md:min-w-[700px] bg-gray-50">
-        <UserProfileForm
-          currentUser={currentUser}
-          onSave={onCheckout}
-          isLoading={isGetUserLoading}
-          title="Confirm Delivery Details"
-          buttonText="Continue to payment"
-        />
-      </DialogContent>
-    </Dialog>
+    <div className="flex justify-center mt-4">
+      <form>
+        <div ref={scriptContainerRef}></div>
+      </form>
+    </div>
   );
 };
 
