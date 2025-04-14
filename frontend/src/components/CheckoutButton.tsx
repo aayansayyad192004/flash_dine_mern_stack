f3c2de7837225bdb1c79e60090604ad3fc2cda7d
aayansayyad192004 <aayansayyad19@gmail.com>
2025-04-14 20:08:52 +0530

    project updated

--- frontend/src/components/CheckoutButton.tsx
+++ frontend/src/components/CheckoutButton.tsx
@@ -36,11 +36,8 @@ const CheckoutButton = ({ isLoading }: Props) => {
 
   return (
     <form>
-      <script
-        src="https://checkout.razorpay.com/v1/payment-button.js"
-        data-payment_button_id="pl_QHS8pZKyf4PAGY"
-        async
-      />
+      <script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_QHS8pZKyf4PAGY" async>
+       </script> 
     </form>
   );
 };
